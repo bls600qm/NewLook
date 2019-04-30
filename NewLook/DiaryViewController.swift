43694a57209ab125b658c203fc2d58972f29a3a8
Rika Sumitomo <lmcmacf@gmail.com>
2019-04-30 21:24:37 +0900

    カメラ関連をDiaryViewControllerに移行 写真選択と表示まで．保存はできてない

--- NewLook/DiaryViewController.swift
+++ NewLook/DiaryViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 import RealmSwift
 
-class DiaryViewController: UIViewController {
+class DiaryViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     var date: String!
     
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var contextTextField: UITextField!
+    //@IBOutlet var photoButton: UIButton!
+    @IBOutlet var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +25,25 @@ class DiaryViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Realmから値を読み込む
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        dateLabel.text = date
+        
+        DispatchQueue(label: "background").async {
+            let realm = try! Realm()
+            
+            if let savedDiary = realm.objects(Diary.self).filter("date == '\(self.date!)'").last {
+                let context = savedDiary.context
+                DispatchQueue.main.async {
+                    self.contextTextField.text = context
+                }
+            }
+        }
+        
     }
     
     @IBAction func saveButtonPushed(_ sender: UIButton) {
@@ -50,6 +71,85 @@ class DiaryViewController: UIViewController {
     //改行で入力確定
     @IBAction func memo(_ sender: Any) {
     }
+    
+    
+    //カメラの処理
+    //アルバムから選択かカメラで撮影かを選択するAlertControllerを出す
+    @IBAction func showAlert(_ sender: Any) {
+        let actionSheet = UIAlertController(title: "画像の選択", message: nil, preferredStyle: UIAlertController.Style.actionSheet)
+        
+        let action1 = UIAlertAction(title: "アルバムから選択", style: UIAlertAction.Style.default, handler: {
+            (action: UIAlertAction!) in
+            print("アルバムから選択")
+            self.tappedlibrary()
+        })
+        
+        let action2 = UIAlertAction(title: "カメラで撮影", style: UIAlertAction.Style.default, handler: {
+            (action: UIAlertAction!) in
+            print("カメラで撮影")
+            self.tappedcamera()
+            
+        })
+        
+        let cancel = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.default, handler: {
+            (action: UIAlertAction!) in
+            print("キャンセルをタップした時の処理")
+        })
+        
+        actionSheet.addAction(action1)
+        actionSheet.addAction(action2)
+        actionSheet.addAction(cancel)
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    //アルバムから選択するメソッド
+    func tappedlibrary() {
+        let sourceType:UIImagePickerController.SourceType =
+            UIImagePickerController.SourceType.photoLibrary
+        
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerController.SourceType.photoLibrary){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+            self.present(cameraPicker, animated: true, completion: nil)
+        }
+        else{
+            print("error")
+        }
+    }
+    //カメラを起動するメソッド
+    func tappedcamera() {
+        let sourceType:UIImagePickerController.SourceType =
+            UIImagePickerController.SourceType.camera
+        // カメラが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerController.SourceType.camera){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+        }
+        else{
+            print("error")
+        }
+    }
+    // 撮影がキャンセルされた時に呼ばれる
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    //撮影が完了した時に呼ばれる
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        self.dismiss(animated: true, completion: nil)
+        //画像を出力
+        photoImageView.image = info[.originalImage] as? UIImage
+    }
+
     
     
 }
--- NewLook/ViewController.swift
+++ NewLook/ViewController.swift
@@ -20,7 +20,7 @@ extension UIColor {
     }
 }
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
 
     let dateManager = DateManager()
     let daysPerWeek: Int = 7
@@ -180,88 +180,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     
     
-    
-    
-    
-    
-    //カメラの処理
-    //アルバムから選択かカメラで撮影かを選択するAlertControllerを出す
-    @IBAction func showAlert(_ sender: Any) {
-        let actionSheet = UIAlertController(title: "画像の選択", message: nil, preferredStyle: UIAlertController.Style.actionSheet)
-        
-        let action1 = UIAlertAction(title: "アルバムから選択", style: UIAlertAction.Style.default, handler: {
-            (action: UIAlertAction!) in
-            print("アルバムから選択")
-            self.tappedlibrary()
-        })
-        
-        let action2 = UIAlertAction(title: "カメラで撮影", style: UIAlertAction.Style.default, handler: {
-            (action: UIAlertAction!) in
-            print("カメラで撮影")
-            self.tappedcamera()
-            
-        })
-        
-        let cancel = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.default, handler: {
-            (action: UIAlertAction!) in
-            print("キャンセルをタップした時の処理")
-        })
-        
-        actionSheet.addAction(action1)
-        actionSheet.addAction(action2)
-        actionSheet.addAction(cancel)
-        
-        self.present(actionSheet, animated: true, completion: nil)
-    }
-    
-    //アルバムから選択するメソッド
-    func tappedlibrary() {
-        let sourceType:UIImagePickerController.SourceType =
-            UIImagePickerController.SourceType.photoLibrary
-
-        if UIImagePickerController.isSourceTypeAvailable(
-            UIImagePickerController.SourceType.photoLibrary){
-            // インスタンスの作成
-            let cameraPicker = UIImagePickerController()
-            cameraPicker.sourceType = sourceType
-            cameraPicker.delegate = self
-            self.present(cameraPicker, animated: true, completion: nil)
-        }
-        else{
-            print("error")
-        }
-    }
-    //カメラを起動するメソッド
-    func tappedcamera() {
-        let sourceType:UIImagePickerController.SourceType =
-            UIImagePickerController.SourceType.camera
-        // カメラが利用可能かチェック
-        if UIImagePickerController.isSourceTypeAvailable(
-            UIImagePickerController.SourceType.camera){
-            // インスタンスの作成
-            let cameraPicker = UIImagePickerController()
-            cameraPicker.sourceType = sourceType
-            cameraPicker.delegate = self
-            self.present(cameraPicker, animated: true, completion: nil)
-            
-        }
-        else{
-            print("error")
-        }
-    }
-    // 撮影がキャンセルされた時に呼ばれる
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-    }
-
-    //撮影が完了した時に呼ばれる
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        self.dismiss(animated: true, completion: nil)
-         //画像を出力
-        //photoImageView.image = info[.originalImage] as? UIImage
-    }
-
-
 }
 //黄色いエラーをたくさん直した _抜けてるとか
 //書き方の順番
